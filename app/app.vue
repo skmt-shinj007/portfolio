7df92e29c226a3edcbab5a7d5cb348f7ae4477ae
sakamoto shinji <shinj0712.dev@gmail.com>
2022-01-29 22:23:19 +0900

    globalのSCSSファイルを追加

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,30 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <!-- ヘッダー -->
+    <default-header/>
+
+    <!-- メイン -->
+    <main>
+      <NuxtPage/>
+    </main>
+
+    <!-- フッター -->
+    <default-footer/>
   </div>
 </template>
+
+<script>
+import DefaultHeader from "./components/header/DefaultHeader.vue";
+import DefaultFooter from "./components/footer/DefaultFooter.vue";
+
+export default {
+  components: {
+    DefaultHeader,
+    DefaultFooter,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -9,11 +9,33 @@ export default defineNuxtConfig({
         protocol: 'ws',
         host: 'localhost'
       }
-    }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          aourceMap: false,
+          additionalData: `@import "@/assets/scss/_modules.scss";`,
+        }
+      }
+    },
   },
 
   // Typescript設定
   typescript: {
     strict: true
   },
+
+  // scss
+  css: [
+    '@/assets/scss/main.scss',
+  ],
+
+  meta: {
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    ],
+  },
+
+  // JAMSTACK設定
+  // target: 'static',
 })
